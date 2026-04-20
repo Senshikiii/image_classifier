b61fa337cba54e2fc7a284a88b27787e3715d4ba
Senshiki <yvessenshiki@gmail.com>
2026-04-20 21:01:50 +0530

    Just one more thing and we're ready to play with thiss

--- src/main.py
+++ src/main.py
@@ -25,9 +25,8 @@ test_data = datasets.MNIST(
 train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
 
 # don't shuffle so the results will be consistent
-# it'll be easy to edit stuff if the configs are at the top
 test_dataloader = DataLoader(test_data, batch_size=64, shuffle=False)
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+
 
 class NeuralNetwork(nn.Module):  # i mean it's jus a class 
     def __init__(self): # we pretty much put all our layers here
@@ -52,17 +51,49 @@ model = NeuralNetwork().to(device)
 print(model)
 # 1e-3 --> 0.001
 learning_rate = 1e-3 
-batch_size 64
+batch_size = 64
 epochs = 5
 
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
 loss_fn = nn.CrossEntropyLoss()
 
+def training_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) item in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        #figures out how much each weight contributed to the loss
+        # actually applies the nudges
+        # resets the graidents
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader, model, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches  = len(dataloader)
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+          print(f"Test error: \n Accuracy: {(100*correct}:>0.1f)%, Avg loss: {test_loss:>8f} \n")
 
 
 
-
-
-
+# btw pred = model snippet does the forward pass thing, loss function does its job
 
 
 
